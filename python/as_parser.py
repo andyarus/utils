3eba91c5f40751ea77ec6cbd2e44ba135cb1c8dc
andyarus <andyarus@users.noreply.github.com>
2018-04-02 12:43:51 +0500

    Update as_parser.py

--- python/as_parser.py
+++ python/as_parser.py
@@ -19,6 +19,7 @@ import optparse
 
 skipIPv6 = True
 
+# RIR - Regional Internet Registry
 # RIRs = [
 # 	'afrinic', # AFRINIC  (African Network Information Center)                     http://www.afrinic.net/ whois.afrinic.net
 # 	'arin',    # APNIC    (Asia-Pacific Network Information Centre)                http://www.apnic.net/   whois.apnic.net
@@ -27,58 +28,74 @@ skipIPv6 = True
 # 	'ripe'     # RIPE NCC (Réseaux IP Européens Network Coordination Centre)       http://www.ripe.net/    whois.ripe.net
 # ]
 
-parser = optparse.OptionParser()
-parser.add_option("--as", dest="autonomousSystems",
-                  help="List of autonomous systems (one or more separated by comma without whitespaces or use '')")
-(options, args) = parser.parse_args()
+tip_msg = 'specify --as=List of autonomous systems (one or more separated by comma without whitespaces or use \'\'). Example:'
+tip_msg += 'python ' + sys.argv[0] + ' --as=AS28890 or python ' + sys.argv[0] + ' --as=AS28890,AS12668'
 
-if not options.autonomousSystems:
-	print 'specify --as=List of autonomous systems (one or more separated by comma without whitespaces or use \'\'). Example:'
-	print 'python ' + sys.argv[0] + ' --as=AS28890 or python ' + sys.argv[0] + ' --as=AS28890,AS12668'
+def parse_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("--as", dest="autonomousSystems",
+	                  help="List of autonomous systems (one or more separated by comma without whitespaces or use '')")
+	(options, args) = parser.parse_args()
+
+	if not options.autonomousSystems:
+		print tip_msg
+		sys.exit()
+
+	AS = [] # AS - autonomous systems
+	AStmp = options.autonomousSystems.split(',')
+	for system in AStmp:
+		if not system.strip():
+			continue
+		if not system.strip().lower().startswith('as'):
+			system = 'AS' + system.strip()
+		AS.append(system)
+
+	return AS
+
+def parse_autonomous_systems(AS):
+	output = ''
+	for system in AS:
+		output += subprocess.check_output("whois -ra -- '-T route,route6 -i origin " + system + "' | grep route | awk '{print$2}'", shell=True)
+		# -r                     turn off recursive look-ups for contact information
+		# -a                     also search all the mirrored databases
+		# whois -ra -- '-T route,route6 -i origin AS28890' | grep route | awk '{print$2}	
+		output += '\n'
+
+	output = output.strip()
+	if not output:
+		return 'whois return empty answer'
+		#sys.exit()
+
+	subnets = []
+	resultSubnets = []
+	subnetsTmp = output.split('\n')
+	for subnet in subnetsTmp:
+		subnet = subnet.strip()
+		if subnet and ('.' in subnet or ':' in subnet):
+			if ':' not in subnet: # IPv4
+				subnets.append(netaddr.IPNetwork(subnet))
+		elif not skipIPv6: # IPv6
+			print 'IPv6:' + subnet
+
+	# merge networks
+	subnets = netaddr.cidr_merge(subnets)
+
+	for subnet in subnets:
+		resultSubnets.append(str(subnet))
+
+	#print 'result subnets:'
+	#for subnet in resultSubnets:
+	#	print subnet
+
+	return '\n'.join(resultSubnets)
+
+AS = parse_arguments()
+if not AS:
+	print tip_msg
 	sys.exit()
 
-AS = [] # AS - autonomous systems
-AStmp = options.autonomousSystems.split(',')
-for system in AStmp:
-	if not system.strip():
-		continue
-	if not system.strip().lower().startswith('as'):
-		system = 'AS' + system.strip()
-	AS.append(system)
-
-output = ''
-for system in AS:
-	output += subprocess.check_output("whois -ra -- '-T route,route6 -i origin " + system + "' | grep route | awk '{print$2}'", shell=True)
-	# -r                     turn off recursive look-ups for contact information
-	# -a                     also search all the mirrored databases
-	# whois -ra -- '-T route,route6 -i origin AS28890' | grep route | awk '{print$2}	
-	output += '\n'
-
-output = output.strip()
-if not output:
-	print 'whois return empty answer'
-	sys.exit()
-
-subnets = []
-resultSubnets = []
-subnetsTmp = output.split('\n')
-for subnet in subnetsTmp:
-	subnet = subnet.strip()
-	if subnet and ('.' in subnet or ':' in subnet):
-		if ':' not in subnet: # IPv4
-			subnets.append(netaddr.IPNetwork(subnet))
-	elif not skipIPv6: # IPv6
-		print 'IPv6:' + subnet
-
-# merge networks
-subnets = netaddr.cidr_merge(subnets)
-
-for subnet in subnets:
-	resultSubnets.append(str(subnet))
-
-#print 'result subnets:'
-for subnet in resultSubnets:
-	print subnet
+result = parse_autonomous_systems(AS)
+print result
 
 
 
@@ -88,7 +105,7 @@ for subnet in resultSubnets:
 # vk.com # AS-VKONTAKTE
 # AS47541
 # AS47542
-# AS28709
+# AS28709 # not used
 
 # ok.ru # mail.ru
 # AS47764
